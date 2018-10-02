c41a2583727d0f78f039393b74794c17f5858c19
Mosh-Media <mo@mosh-media.com>
2018-10-01 18:08:19 -0700

    Clash API

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -12,9 +12,10 @@ export class CardsComponent implements OnInit {
   public id: string;
   public maxlevel: number;
  cards: any[];
-  fetchData() {
+ status: Boolean = false;
+  async fetchData() {
 
-    fetch('https://api.clashroyale.com/v1/cards', {
+    await fetch('https://api.clashroyale.com/v1/cards', {
       headers:{
         'authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImExNTQ0MzVjLTQzMjAtNDM3Yi1hZWY3LTQ4ZDUwNWI3NDllOCIsImlhdCI6MTUzODQxOTIwNCwic3ViIjoiZGV2ZWxvcGVyLzFkMjBiZWI3LWYxMjctNjVkMi03NzYyLTczZTkyMDhjYzZkZiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxNzQuMjE0LjIuMTYiXSwidHlwZSI6ImNsaWVudCJ9XX0.ZCTMgY6wQmTY0LAEaforUW4Qzm4RM0jak9o_nU88XeBxrGBfT_fAZNLUkRmcVcCJ1Ww2rE49RYx2nrY4kuPsSA',
         'Content-Type': 'application/json'
@@ -29,7 +30,12 @@ export class CardsComponent implements OnInit {
     err => console.log(err));
 
   }
-
+   hoverIn(e){
+    this.status = true;  
+  }
+  hoverOut(e){
+    this.status = false;  
+  }
   ngOnInit() {
 this.fetchData();
   }
