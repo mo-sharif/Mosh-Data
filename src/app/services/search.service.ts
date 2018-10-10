22f41e09aeeaac90a16ca7fbbf6bd63991b0f368
Mosh-Media <mo@mosh-media.com>
2018-10-09 18:12:19 -0700

    fixed music search

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http , Jsonp, Response} from '@angular/http';
 import {ItemsComponent} from '../classes/items/items.component'
 import { Observable } from 'rxjs';
 import {
@@ -12,11 +12,11 @@ import {
 @Injectable()
 export class SearchService {
   apiRoot: string = "https://itunes.apple.com/search";
-  constructor(private http: Http) {}
+  constructor(private http: Http, private jsonp: Jsonp) {}
 
   search(term: string): Observable<ItemsComponent[]> {
-    let apiURL = `${this.apiRoot}?term=${term}&media=music&limit=20`;
-    return this.http.get(apiURL).pipe(
+    let apiURL = `${this.apiRoot}?term=${term}&media=music&limit=20&callback=JSONP_CALLBACK`;
+    return this.jsonp.request(apiURL).pipe(
       map(res => {
         return res.json().results.map(item => {
           return new ItemsComponent(
