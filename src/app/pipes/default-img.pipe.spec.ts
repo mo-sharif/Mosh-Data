e44b31213cb60fbaf7eb0d22ba9de073cf5bcd33
Mosh-Media <mo@mosh-media.com>
2018-10-09 19:36:50 -0700

    Production files

--- src/app/pipes/default-img.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DefaultImgPipe } from './default-img.pipe';
-
-describe('DefaultImgPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DefaultImgPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
